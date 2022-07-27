22ac85d1a83c62ece3077f7b04cd3c424150c2d5
Varun Santhanam <talkto@vsanthanam.com>
2022-07-26 19:52:25 -0700

    Add Static Site Generator

---
+++ generate-static-site.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+if [[ "$branch" != "main" ]]; then
+    echo "Invalid Branch"
+else
+    git branch -D gh-pages
+    git checkout -b gh-pages
+    rm -rf ~/Library/Developer/Xcode/DerivedData
+    xcodebuild docbuild -scheme SafariView \
+    -destination generic/platform=iOS \
+    OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path SafariView/docs --output-path docs"
+    echo "theme: jekyll-theme-modernist" > _config.yml
+    git add .
+    git commit -m 'Synchronize Hompage & Publish Documentation'
+    git push -f -u origin gh-pages
+    git checkout main
+    git branch -D gh-pages
+    rm -rf docs
+    echo "Website Updated!"
+fi
