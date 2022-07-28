7fe1a9804ae0495915590d52d60eb20ade6f5362
Varun Santhanam <talkto@vsanthanam.com>
2022-07-28 08:34:23 -0700

    Update Docs

--- Sources/SafariView/SafariView.swift
+++ Sources/SafariView/SafariView.swift
@@ -31,7 +31,11 @@ import UIKit
 ///
 /// A `SafariView` is a wrapper around [`SFSafariViewController`](https://developer.apple.com/documentation/safariservices/sfsafariviewcontroller) for use within SwiftUI applications.
 ///
-/// You can present a `SafariView` using one of our provided view modifers, like so:
+/// The view includes Safari features such as Reader, AutoFill, Fraudulent Website Detection, and content blocking. The user's activity and interaction with `SafariView` are not visible to your app, which cannot access AutoFill data, browsing history, or website data. You do not need to secure data between your app and Safari. If you would like to share data between your app and Safari, so it is easier for a user to log in only one time, use [`ASWebAuthenticationSession`](https://developer.apple.com/documentation/authenticationservices/aswebauthenticationsession) instead.
+///
+/// - Important: In accordance with [App Store Review Guidelines](https://developer.apple.com/app-store/review/guidelines/), this view must be used to visibly present information to users; the view may not be hidden or obscured by other views or layers. Additionally, an app may not use `SafariView` to track users without their knowledge and consent.
+///
+/// You can present a `SafariView` using one of our provided view modifes, like so:
 ///
 /// ```swift
 /// import Foundation
